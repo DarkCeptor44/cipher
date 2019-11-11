7fa5b775f1a5b0a3715525753ef9680727cae1e4
129fc5e32c631b5007cb2cd8d45d416002b42bab <16278483+DarkCeptor44@users.noreply.github.com>
2019-11-11 17:08:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Cipher
+
+Encrypts and Decrypts any `.txt` file in the current directory when a password is provided (unless the names of the files are specified in the `EXCEPTIONS` array).
+Useful to hide text from people with either physical or remote access to your machine, you can use for diaries (just an example).
+
+## How to use
+
+ 1. Download `cipher.py` to your machine.
+ 2. Open a command prompt on the directory where `cipher.py` is:
+ 3. Type `python` , press Enter, type `import os; os.urandom(16)` , press Enter and copy the line that shows up (it looks like `b'xxxxxxxxxxxxxxxxxxx'` )
+ 4. Open `cipher.py` and look for line 58 where it says `salt = b'xxxxxxxxxxxxxxx'` , then just paste that line you copied after the `=` .
+ 5. Save it and run it to encrypt the `.txt` files that are in the same directory.
+
+## Notes
+
+* You can use any password on encryption but if you try to decrypt with a different password than you used it's gonna give an error.
+* I recommend use of [auto-py-to-exe](https://github.com/brentvollebregt/auto-py-to-exe) to compile `.py` in `.exe` which makes it harder for someone to see the code.
+
+### This script is meant for less tech-savvy people as it's not highly advanced but can be modified to whatever you need
---
+++ cipher-win.py
@@ -0,0 +1,84 @@
+import os
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet, InvalidToken
+
+EXCEPTIONS = ["cipher.py", "cipher.exe"]
+
+
+def encode(filename, fernet):
+    if filename.endswith(".cipher"):
+        raise Exception("can't encode an already encoded file")
+
+    print("encoding '%s' now" % filename)
+
+    with open(filename, "rb") as f:
+        data = f.read()
+
+    encrypted = fernet.encrypt(data)
+
+    with open(filename, "wb") as f:
+        f.write(encrypted)
+
+    os.rename(filename, get_filename_without_extension(filename) + ".cipher")
+
+
+def decode(filename, fernet):
+    if filename.endswith(".txt"):
+        raise Exception("can't decode an already decoded file")
+
+    print("decoding '%s' now" % filename)
+
+    with open(filename, "rb") as f:
+        data = f.read()
+
+    decrypted = fernet.decrypt(data)
+
+    with open(filename, "wb") as f:
+        f.write(decrypted)
+
+    os.rename(filename, get_filename_without_extension(filename) + ".txt")
+
+
+def decide(filename, f):
+    if filename.endswith(".txt"):
+        encode(filename, f)
+    elif filename.endswith(".cipher"):
+        decode(filename, f)
+
+
+def get_filename_without_extension(filename):
+    return os.path.splitext(filename)[0]
+
+
+def get_key_from_password(password_provided):
+    password = password_provided.encode()
+    salt = b"\xec\xdc\xd5\xd2:J\xe2\x9e?\xd6\x1f\x0b^\xc4Hs"
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend(),
+    )
+    return base64.urlsafe_b64encode(kdf.derive(password))
+
+
+if __name__ == "__main__":
+    try:
+        os.system("color b")
+        files = os.listdir(".")
+        key = get_key_from_password(input("Insert key to encrypt/decrypt the files: "))
+        fernet = Fernet(key)
+
+        for file in files:
+            if file not in EXCEPTIONS:
+                decide(file, fernet)
+    except InvalidToken:
+        print("Invalid key!")
+    except Exception as ex:
+        print("Error: %s" % str(ex))
+    finally:
+        os.system("timeout /t 3 >nul")
